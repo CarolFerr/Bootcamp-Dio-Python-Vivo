0d9b4804ef23293a77e5e0fb0351bad2870d6254
CarolFerr <114115953+CarolFerr@users.noreply.github.com>
2024-04-15 14:28:20 -0300

    Commit exercicios resolvidos

---
+++ lista-equipamentos.py
@@ -0,0 +1,18 @@
+# TODO: Crie uma Lista 'itens' para armazenar os equipamentos:
+itens = []
+for i in range(3):
+  item = input()
+  itens.append(item)
+
+# TODO: Crie um loop para solicita os itens ao usuário:
+
+# TODO: Solicite o item e armazena na variável "item":
+
+# TODO: Adicione o item à lista "itens":
+
+
+# Exibe a lista de itens
+print("Lista de Equipamentos:")
+for item in itens:
+    # Loop que percorre cada item na lista "itens"
+    print(f"- {item}")
